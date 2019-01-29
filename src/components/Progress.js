437aefa4151f50985b38ddcff3ee25813f22264c
Vlad Sazonov <vladsazon27@gmail.com>
2019-01-29 21:52:01 +0300

    Refactored data scheme and added styles

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,30 +1,35 @@
-import React from 'react'
-import Result from './Result'
-import styled from 'styled-components'
+import React from "react";
+import Result from "./Result";
+import styled from "styled-components";
 
-function Progress({ title, text, icon, results }) {
+function Progress({ title, text, icon, results, status }) {
   return (
     <Container>
       <DotContainer>
-        <Dot results={results} />
+        <Dot status={status} />
       </DotContainer>
       <LeftColumn>
         <Image src={icon} alt="icon" />
         <TextContainer>
-          <p>{title}</p>
+          <Title>{title}</Title>
           <Text>{text}</Text>
         </TextContainer>
+        {status === "notSubmitted" && (
+          <Badge>
+            <BadgeText>Новый</BadgeText>
+          </Badge>
+        )}
       </LeftColumn>
       <ResultsContainer>
         {results.map((result, id) => (
-          <Result {...result} key={id} /> //bad practice putting id in key
+          <Result {...result} key={id} status={status} /> //bad practice putting id in key
         ))}
       </ResultsContainer>
     </Container>
-  )
+  );
 }
 
-export default Progress
+export default Progress;
 
 const Container = styled.div`
   display: flex;
@@ -32,35 +37,33 @@ const Container = styled.div`
   margin-bottom: 20px;
   width: 1164px;
   max-width: 100%;
-`
+  height: 100px;
+`;
 const DotContainer = styled.div`
   display: flex;
   align-items: center;
-`
+`;
 const Dot = styled.div`
   background: ${props => {
-    return props.results.map(({ status }) => {
-      console.log(status)
-      if (status === 'error') {
-        return `red`
-      }
-      if (status === 'approved') {
-        return `#8ABF49`
-      }
-      if (status === 'waiting') {
-        return `orange`
-      }
-      if (status === 'notSubmitted') {
-        return `gray`
-      }
-    })
+    if (props.status === "error") {
+      return "red";
+    }
+    if (props.status === "approved") {
+      return "#8ABF49";
+    }
+    if (props.status === "waiting") {
+      return "orange";
+    }
+    if (props.status === "notSubmitted") {
+      return "gray";
+    }
   }};
   border-radius: 100%;
   height: 20px;
   width: 20px;
   position: relative;
   right: 18px;
-`
+`;
 const LeftColumn = styled.div`
   display: flex;
   align-items: center;
@@ -69,24 +72,38 @@ const LeftColumn = styled.div`
   border-radius: 2px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   padding: 20px;
-`
+  position: relative;
+`;
 const Image = styled.img`
   width: 50px;
   height: 50px;
-`
+`;
 const TextContainer = styled.div`
   display: flex;
   flex-flow: column;
   align-items: baseline;
   margin-left: 25px;
-`
+`;
 const Text = styled.p`
   text-align: start;
-`
-
+`;
+const Title = styled.p`
+  font-weight: bold;
+`;
+const Badge = styled.div`
+  background: #2895f2;
+  position: absolute;
+  right: 7px;
+  top: 7px;
+  padding: 5px;
+  border-radius: 3px;
+`;
+const BadgeText = styled.span`
+  color: white;
+`;
 const ResultsContainer = styled.div`
   display: flex;
   flex-flow: column;
   width: 50%;
   margin-left: 30px;
-`
+`;
--- src/components/ProgressList.js
+++ src/components/ProgressList.js
@@ -27,35 +27,35 @@ const Container = styled.div`
 
 const Line1 = styled.div`
   width: 6px;
-  height: 25%;
+  height: 27%;
   position: absolute;
   left: -11px;
-  top: 75px;
+  top: 45px;
   background: linear-gradient(180deg, rgba(98,193,57,1) 0%, rgba(235,110,22,1) 100%);
 `
 const Line2 = styled.div`
   width: 6px;
-  height: 25%;
+  height: 28%;
   background-color: blue;
   position: absolute;
   left: -11px;
-  top: 235px;
+  top: 147px;
   background: linear-gradient(180deg, rgba(235,110,22,1) 0%, rgba(254,218,71,1) 100%);
 `
 const Line3 = styled.div`
   width: 6px;
-  height: 25%;
+  height: 28%;
   background-color: lightgreen;
   position: absolute;
   left: -11px;
-  bottom: 99px;
+  bottom: 55px;
   background: linear-gradient(180deg, rgba(254,218,71,1) 0%, rgba(76,143,224,1) 100%);
 `
 const Line4 = styled.div`
   width: 6px;
-  height: 25%;
+  height: 20%;
   background-color: gray;
   position: absolute;
   left: -11px;
-  bottom: -65px;
+  bottom: -20px;
 `
