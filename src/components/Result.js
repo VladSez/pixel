4de69fc3f59c497cf3ec3d520390d62c9d546dc8
Vlad Sazonov <vladsazon27@gmail.com>
2019-01-29 22:37:57 +0300

    Style fixes

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,41 +1,76 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { Check } from "styled-icons/boxicons-regular/Check";
+import { Flag } from "styled-icons/fa-solid/Flag";
+import { Clock } from "styled-icons/fa-regular/Clock";
+import { Circle } from "styled-icons/feather/Circle";
 
-function Result({ text, icon, status }) {
+function Result({ text, status }) {
   return (
     <Container>
-      <Image src={icon} alt="status" />
+      {status === "approved" && <StyledCheck />}
+      {status === "error" && <StyledFlag />}
+      {status === "waiting" && <StyledClock />}
+      {status === "notSubmitted" && <StyledCircle />}
       <Status status={status}>{text}</Status>
     </Container>
-  )
+  );
 }
 
-export default Result
+export default Result;
 
+const StyledCircle = styled(Circle)`
+  color: gray;
+  width: 16px;
+  height: 16px;
+  position: relative;
+  top: 2px;
+  margin-right: 10px;
+`;
+const StyledCheck = styled(Check)`
+  color: lightgreen;
+  width: 16px;
+  height: 16px;
+  position: relative;
+  top: 2px;
+  margin-right: 10px;
+`;
+const StyledFlag = styled(Flag)`
+  color: tomato;
+  width: 16px;
+  height: 16px;
+  position: relative;
+  top: 2px;
+  margin-right: 10px;
+`;
+const StyledClock = styled(Clock)`
+  color: orange;
+  width: 16px;
+  height: 16px;
+  position: relative;
+  top: 2px;
+  margin-right: 10px;
+`;
 const Container = styled.div`
   display: flex;
   align-items: center;
   cursor: pointer;
-`
-const Image = styled.img`
-  width: 15px;
-  height: 15px;
-  margin-right: 5px;
-`
+`;
+
 const Status = styled.p`
   text-align: start;
   color: ${props => {
-    if (props.status === 'error') {
-      return 'red'
+    if (props.status === "error") {
+      return "red";
     }
-    if (props.status === 'approved') {
-      return '#8ABF49'
+    if (props.status === "approved") {
+      return "#8ABF49";
     }
-    if (props.status === 'waiting') {
-      return 'orange'
+    if (props.status === "waiting") {
+      return "orange";
     }
-    if (props.status === 'notSubmitted') {
-      return 'gray'
+    if (props.status === "notSubmitted") {
+      return "gray";
     }
   }};
-`
+`;
